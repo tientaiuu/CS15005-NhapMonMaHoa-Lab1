b9dadfbcec90f73f287b6aeda77a592149d6c8d6
TranHuuTai <huutai22120318@gmail.com>
2024-11-07 11:28:20 +0700

    generate private key

---
+++ generate_private_key/generate_private_key.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+long long generate_private_key(long long p) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<long long> dis(2, p - 2);
+    return dis(gen);
+}
+
+int main() {
+    long long p = 32416190071; 
+    long long private_key = generate_private_key(p);
+    cout << "Khoa rieng duoc sinh: " << private_key << "\n";
+    return 0;
+}
